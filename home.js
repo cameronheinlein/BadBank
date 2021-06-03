edc80333b4f37b3191f7a49a5139370ed22045a5
cameronheinlein <76449880+cameronheinlein@users.noreply.github.com>
2021-06-03 16:23:50 -0400

    adds context props and create account

--- home.js
+++ home.js
@@ -1,5 +1,12 @@
 function Home(){
     return(
-        <h1>Home</h1>
+        <Card
+            bgcolor="primary"
+            txtcolor="black"
+            header="BadBank Landing Page"
+            title="Welcome to the bank"
+            text="You can use this bank"
+            body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+        />
     );
 }
