57054b9d3f831c0089305c6553ea5de92404aadd
cameronheinlein <76449880+cameronheinlein@users.noreply.github.com>
2021-06-03 14:38:48 -0400

    adds card component

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ function Spa(){
     return (
         <HashRouter>
             <NavBar/>
-            <Route path="/" exact component={Home} />
-            <Route path="/CreateAccount/" component={CreateAccount} />
-            <Route path="/login/" component={Login} />
-            <Route path="/deposit/"  component={Deposit} />
-            <Route path="/withdraw/" component={Withdraw} />
-            <Route path="/balance/" component={Balance} />
-            <Route path="/alldata/" component={AllData} />
+            <UserContext.Provider value={{users:[{name:'Cammi', email:'cameron.heinlein@gmail.com', password:'secret', balance:100}]}}>
+                <Route path="/" exact component={Home} />
+                <Route path="/createaccount/" component={CreateAccount} />
+                <Route path="/login/" component={Login} />
+                <Route path="/deposit/"  component={Deposit} />
+                <Route path="/withdraw/" component={Withdraw} />
+                <Route path="/balance/" component={Balance} />
+                <Route path="/alldata/" component={AllData} />
+            </UserContext.Provider>
         </HashRouter>
     );
 }
