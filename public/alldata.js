1af1161aa1825bc74b919ecefaaa3dc131e17da4
cameronheinlein <76449880+cameronheinlein@users.noreply.github.com>
2021-08-30 22:23:56 -0400

    adds backend refactor

--- public/alldata.js
+++ public/alldata.js
@@ -1,11 +1,24 @@
 function AllData(){
-  const ctx = React.useContext(UserContext);
-return (
-  <Card
-    bgcolor="secondary"
-    header="All Data in Store"
-    status={status}
-    body={JSON.stringify(ctx)}
-      />
-)
-}
+    const [data, setData] = React.useState('');    
+
+    React.useEffect(() => {
+        
+        // fetch all accounts from API
+        fetch('/account/all')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(JSON.stringify(data));                
+            });
+
+    }, []);
+
+    return (
+    <Card
+        bgcolor="secondary"
+        header="All Data in Store"
+        status={status}
+        body={data}
+          />
+    )
+    }
